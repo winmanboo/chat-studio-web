a4c22163574447ad4310493ee3e64b60dff8beb9
winmanboo <winmanboo2332@gmail.com>
2025-08-18 20:34:17 +0800

    feat: dev模式开启代理

--- next.config.js
+++ next.config.js
@@ -4,7 +4,15 @@ const nextConfig = {
   transpilePackages: ['antd', '@ant-design/icons', '@ant-design/x'],
   experimental: {
     optimizePackageImports: ['antd']
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
